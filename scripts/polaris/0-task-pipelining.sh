7f04f886e73215810c6fdbcab9f124e6553316b2
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-03-13 02:05:47 +0000

    Rerun task pipelining on Polaris with fixed submit time

--- scripts/polaris/0-task-pipelining.sh
+++ scripts/polaris/0-task-pipelining.sh
@@ -9,7 +9,7 @@ echo "Redis started on port $PORT."
 
 ARGS="--executor dask --dask-workers 2 "
 ARGS+="--ps-connector redis --ps-host localhost --ps-port $PORT "
-ARGS+="--submission-method no-proxy sequential pipelined "
+ARGS+="--submission-method sequential-no-proxy sequential-proxy pipelined-proxy-future "
 ARGS+="--task-chain-length 8 "
 ARGS+="--task-data-bytes 10000000 "
 ARGS+="--task-overhead-fractions 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 "
