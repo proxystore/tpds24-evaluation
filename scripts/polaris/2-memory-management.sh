b2b4e791b56cf478c40deb6ff93aca869e472a6b
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-03-28 21:50:24 +0000

    Rerun workflow memory on Polaris

--- scripts/polaris/2-memory-management.sh
+++ scripts/polaris/2-memory-management.sh
@@ -20,8 +20,8 @@ ARGS+="--run-dir data/$SYSTEM/2-memory-management "
 echo "Using args: $ARGS"
 
 python -m psbench.run.workflow_memory $ARGS --data-management none
-python -m psbench.run.workflow_memory $ARGS --data-management manual-proxy
 python -m psbench.run.workflow_memory $ARGS --data-management owned-proxy
+python -m psbench.run.workflow_memory $ARGS --data-management manual-proxy
 # We run the default-proxy last because it will leave data in the redis
 # server and we don't want that to throw off the memory usage for the
 # prior runs.
